c27c3040c36b0492069912c65469e8ee4e2a8a7f
Byboth Jackson <you@example.com>
2021-09-15 14:40:48 -0500

    add functions

--- Main.py
+++ Main.py
@@ -9,7 +9,33 @@ def topFiveMovies():
     #print fourth movie 
     print("Your Name")
     #print fifth movie 
-    print("Weathering With You")
+    print("Weathering With You")  
+topFiveMovies()
+def bestTacoBellitems():
+    #added first food
+    print("Nacho fries")
+    # added second items
+    print("quesadilla")
+    #added third item
+    print("soft taco")
+bestTacoBellitems()
+def topThreegames():
+    #add first game
+    print("Breath of the wild")
+    #add second game
+    print("stardew valley")
+    #add third game
+    print("Minecraft")
+topThreegames()
+def topThreeAnime():
+#add first anime
+    print("steins:gate")
+#add second anime
+    print("Code Geass")
+#add third anime
+    print("Death note")
+topThreeAnime()
+
 
     
 
