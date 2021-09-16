f79ed898a8868460ee66a8485feb08926d3dd9af
jackbyboth23 <817581@mystma.org>
2021-09-16 15:19:01 -0500

    added madlib

--- Main.py
+++ Main.py
@@ -37,7 +37,29 @@ def topThreeAnime():
 topThreeAnime()
 print (69 // 420)
 def myNameIs(taco):
-    print(taco + "is my name")
+    print(taco + " is my name")
 
 myNameIs("jack")
-myNameis("joe")
+myNameIs("joe")
+def timesTen(num):
+    print(num * 10)
+timesTen(234)
+def addFive(numb):
+    print(numb + 5)
+addFive(34)
+def divTwo(divTwo):
+    print(divTwo / 4)
+divTwo(16)
+def addTwoNumbers(x, y):
+    print(x + y)
+addTwoNumbers(42, 17)
+
+
+
+def madLib(instrument, hours, day, letter, major, number, number2, adjective,  adjective2,fraction, adjective3, instument2,noun, song, adjectived, artist):
+    print("while ive stayed at home ive practiced the" + instrument +
+    "for" + hours + "hours every" + day + "my favorite key signature to practice in is"
+    + letter + major + "it has" + number + "flats and" + number2 + "sharps. I like this key signature because it is" +  
+    adjective + "and"+adjective2+ "ive also gotten awesome at counting time signatures, The time signature i love is"+fraction+ "because it is" +adjective3+"to count. one instrument i want to learn to play while im at home is"+instument2+"because it is,"+adjective4
+    +"and sound like a."+noun+"my"+family+"likes it when i play" +song+"and always gives me a round of applause after my preformances when i return to school, my teacher will be"+adjectived+"of how great im playing my instrument! my teacher might even think im"+artist)
+madLib(trumpet, 4, day, z, major, 69, 420, cool, adjective2, fraction, adjective3, instument2, noun, song, adjectived, artist)
